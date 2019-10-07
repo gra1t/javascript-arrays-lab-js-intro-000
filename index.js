54a75f96723a43a62c5d4d5de3db9d43db33806d
granit <graniitkastrati@gmail.com>
2019-10-07 22:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-var destructivelyAppendKitten = kittens.push()
+var destructivelyAppendKitten = [kittens.push()]
+
